366c088836c194e0a0048f57a63e0a6059171aa6
begunfx <begunfx@usa.net>
2015-11-13 21:55:21 -0800

    Added project assignments to header

--- ThanksgivingCard.py
+++ ThanksgivingCard.py
@@ -1,7 +1,8 @@
 #CST205: Thanksgiving Day Card - Group Project
 #Team: Startup Solutions Inc. SSI
 
-#Brian Begun
-#Wendy Gray
-#Christopher Dixon
-#William Gillihan
+#Brian Begun: Green Screen Border Leaves
+#Wendy Gray: Repeating Background Pattern
+#Christopher Dixon: Header Banner and Thanksgiving Greeting Text
+#William Gillihan: Artify Center Photo
+
