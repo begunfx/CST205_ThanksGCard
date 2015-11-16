a9c3c6bf3120249af8ce0af5ae5e991db5540e0a
begunfx <begunfx@usa.net>
2015-11-15 19:53:41 -0800

    Added Chris functions to main file

--- ThanksgivingCard.py
+++ ThanksgivingCard.py
@@ -221,3 +221,40 @@ def chromakey(pic, background, refX, refY, threshold):
       
   #show(foreground)
   return(foreground)
+  
+  
+############## Christopher Dixon #####################
+# addGreeting
+# ver. 11/15/15
+# 
+# 
+# 
+###################################################### 
+  
+def addGreeting(pic):
+  cardWidth = 700  
+  cardHeight = 500
+  greeting = "Happy Thanksgiving"
+  textShadowColor = makeColor(144, 71, 8)
+  textColor = makeColor(255, 225, 8)
+  textStyle = makeStyle(serif, italic + bold, 48)
+  bannerColor = makeColor(232, 101, 5)
+  addTextWithStyle(pic, 162, 52, greeting, textStyle, textShadowColor)
+  addTextWithStyle(pic, 160, 50, greeting, textStyle, textColor)
+  return pic
+  
+
+############## Christopher Dixon #####################
+# makeBanner
+# ver. 11/15/15
+# 
+# 
+# 
+###################################################### 
+def makeBanner(pic):
+  cardWidth = 700  
+  cardHeight = 500
+  bannerColor = makeColor(232, 101, 5) #used colord from background leaves
+  addRectFilled(pic, 0, 0, cardWidth, int(cardHeight * 0.15), bannerColor)
+  show(pic)
+  return pic    
